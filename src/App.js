200e628ccd95a150a2612e5594ea7d2ff68f0654
Rebecca Viner <rtviner@gmail.com>
2019-01-30 13:40:03 -0700

    update submit to form submit instead of button click and add prevent default

--- src/App.js
+++ src/App.js
@@ -1,19 +1,91 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const DefaultCups = 2;
+
+// input grams of water per 1 cup and desired cups (user input number) and output will be the number of grams of water required to make those desired cups
+const WaterGrams = (cupSizeML = 280, desiredCups) => cupSizeML * desiredCups;
+
+//input waterGrams needed for desiredCups of coffee and goldenRatio (grams of water per 1 gram coffee) and outputs coffee grams required to make desired cups.
+const CoffeeGrams = (waterGrams, goldenRatio = 15) => waterGrams / goldenRatio;
+
+//input coffeeGrams available (user input number) and goldenRatio and outPuts number of cups possible
+const DesiredCups = (coffeeGrams, goldenRatio = 15) => coffeeGrams / goldenRatio;
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cupsKey: '',
+      cups: DefaultCups,
+      ingredients: '',
+      error: null,
+    };
+    this.onSubmitChange = this.onSubmitChange.bind(this);
+    this.onCupsSubmit = this.onCupsSubmit.bind(this);
+  }
+
+  onSubmitChange(event) {
+    this.setState({ cups: event.target.value });
+  }
+
+  onCupsSubmit(event) {
+    const { cups } = this.state;
+    this.setState({ cupsKey: cups });
+    event.preventDefault();
+  }
+
   render() {
+    const { cups, ingredients, error} = this.state;
     return (
       <div className="App">
         <header className="App-header">
-
-
+          <h1>Brew Ratio</h1>
         </header>
+        <main>
+          <div className="interactions">
+            <CupInput 
+              value={cups}
+              onChange={this.onSubmitChange}
+              onSubmit = {this.onCupsSubmit}
+            >
+
+            </CupInput>
+          </div>
+          <Ingredients>
+          </Ingredients>
+        </main>
       </div>
     );
   }
 }
+
+const CupInput = ({value, onChange, onSubmit}) =>
+        <form onSubmit = {onSubmit}>
+          <label>
+            Desired Cups:
+            <input
+              type="text"
+              value={value}
+              onChange={onChange}
+            />
+          </label>
+          <button type="submit" >
+            Submit
+          </button>
+        </form>
+
+const Ingredients = (waterGrams, coffeeGrams) =>
+  <div className="ingredients-result">
+    <div className="coffee">
+     
+      (g)
+    </div>
+    <div className="water"> 
+
+      (g)
+    </div>
+  </div>
 
 export default App;
