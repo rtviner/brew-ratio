785600238d1eaadbdfc5fe4f26df39679b78aad6
Rebecca Viner <rtviner@gmail.com>
2019-09-18 20:37:39 -0600

    update variable names

--- src/increment.js
+++ src/increment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const IncrementOrDecrementButton = ({ id, onClick, text }) => (
     <button
@@ -11,38 +12,38 @@ const IncrementOrDecrementButton = ({ id, onClick, text }) => (
     </button>
 );
 
-// IncrementOrDecrementButton.propTypes = {
-//     id: PropTypes.string,
-//     onClick: PropTypes.func,
-//     text: PropTypes.string
-// };
+IncrementOrDecrementButton.propTypes = {
+    id: PropTypes.string,
+    onClick: PropTypes.func,
+    text: PropTypes.string
+};
 
-const IncrementDecrementSet = ({ name, title, interval, setIntervalTime }) => (
-    <div className="timer" id={`${name}-label`}>
+const IncrementDecrementSet = ({ name, title, value, setQuantity }) => (
+    <div className="incrementDecrement" id={`${name}-label`}>
         <h2>{title}</h2>
         <div className="interactions">
-            <div id={`${name}-length`}> {interval} </div>
+            <div id={`${name}-amount`}> {value} </div>
             <div className="increment">
                 <IncrementOrDecrementButton
                     id={`${name}-increment`}
                     text="+"
-                    onClick={setIntervalTime}
+                    onClick={setQuantity}
                 />
                 <IncrementOrDecrementButton
                     id={`${name}-decrement`}
                     text="-"
-                    onClick={setIntervalTime}
+                    onClick={setQuantity}
                 />
             </div>
         </div>
     </div>
 );
 
-// IncrementDecrementSet.propTypes = {
-//     name: PropTypes.string,
-//     title: PropTypes.string,
-//     interval: PropTypes.number,
-//     setIntervalTime: PropTypes.func
-// };
+IncrementDecrementSet.propTypes = {
+    name: PropTypes.string,
+    title: PropTypes.string,
+    interval: PropTypes.number,
+    setQuantity: PropTypes.func
+};
 
 export { IncrementDecrementSet  };
