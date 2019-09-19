2db4e7d7231e1a665b51c3e1c3abc64f40b1a0c5
Rebecca Viner <rtviner@gmail.com>
2019-09-18 20:36:47 -0600

    add inputButton.js

---
+++ src/inputButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InputButton = ({ className, id, name, value, onClick }) => (
+  <span className="button">
+    <input
+      className = {className}
+      type="button"
+      id={id}
+      name={name}
+      value={value}
+      onClick={onClick}
+    />   
+  </span>
+);
+
+InputButton.propTypes = {
+    className: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onClick: PropTypes.func,
+};
+
+export { InputButton  };
