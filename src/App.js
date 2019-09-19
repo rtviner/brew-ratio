cddb825409d2285f9da2bee7537a10217be66f7c
Rebecca Viner <rtviner@gmail.com>
2019-09-19 15:55:20 -0600

    change incrementDecrementQuantity to updateQuantity and add logic for number input

--- src/App.js
+++ src/App.js
@@ -3,43 +3,66 @@ import { IncrementDecrementSet } from './increment.js';
 import { InputButton } from "./inputButton.js";
 import './App.css';
 
+const DefaultCups = 2;
 const DefaultBrewAmt = 20;
-const DefaultCoffee = 36;
-const DefaultWater = 560;
+const DefaultCoffee = 38;
+const DefaultWater = 592;
 const DefaultRatio = "15.5";
+const waterGramsPerOz = 29.574;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      goldenRatio: DefaultRatio, 
+      goldenRatio: DefaultRatio,
+      cups: DefaultCups,
       brewAmount: DefaultBrewAmt,
       coffeeGrams: DefaultCoffee,
       waterGrams: DefaultWater, 
       error: null,
     };
 
-    this.setQuantity = this.setQuantity.bind(this);
+    this.updateQuantity = this
+      .updateQuantity.bind(this);
+    this.waterOzToGrams = this.waterOzToGrams.bind(this);
+    this.waterGramsToOz = this.waterGramsToOz.bind(this);
+    // this.updateQuantities = this
+    //   .updateQuantities.bind(this);
 
   }
 
+  updateAllQuantities(quantity1, quantity2) {
 
-  setQuantity (event) {
+  };
+
+  updateQuantity (event) {
     const eventInfo = event.target.id.split("-");
     const name = eventInfo[0];
     const currentQuantity  = this.state[name];
     let newQuantity;
 
-    if (eventInfo[1] === "increment" ) {
-      newQuantity = currentQuantity + 1;
+    if (event.target.type === "number") {
+      newQuantity = parseFloat(event.target.value);
     }
 
-    if (eventInfo[1] === "decrement" && currentQuantity > 1) {
-      newQuantity = currentQuantity - 1;
+    if (event.target.type === "button") {
+      if (eventInfo[1] === "increment" ) {
+        newQuantity = currentQuantity + 1;
+      }
+      if (eventInfo[1] === "decrement" && currentQuantity > 1) {
+        newQuantity = currentQuantity - 1;
+      }
     }
+   
     this.setState({ [name]: newQuantity });
-    //Need to recalculate and update other two quantity states that are not name too
+
+    // if (name === "brewAmount") {
+    //   this.setState({ waterGrams: this.waterOzToGrams(newQuantity )});
+    // }
+    // if (name === "waterGrams") {
+    //   this.setState({ brewAmount: this.waterGramsToOz(newQuantity )});
+    // }
   }
 
 //no longer a submit button
@@ -58,24 +81,43 @@ class App extends Component {
 //   }
 
 //this is not being called right now
-  setBrewAmount(coffeeGrams, goldenRatio, servingSize = 280) {
-    const possibleServings = (coffeeGrams * goldenRatio) / servingSize;
-    const roundedPossibleServings = Math.round(possibleServings * 4) / 4;
+  // setServings(coffeeGrams, goldenRatio, servingSize = 280) {
+  //   const possibleServings = (coffeeGrams * goldenRatio) / servingSize;
+  //   const roundedPossibleServings = Math.round(possibleServings * 4) / 4;
 
-    this.setIngredients(roundedPossibleServings, goldenRatio);
+  //   this.setIngredients(roundedPossibleServings, goldenRatio);
+  // }
+
+//convert waterGrams to oz
+  waterGramsToOz (waterGrams) {
+    const brewAmountOz = Math.round(waterGrams / waterGramsPerOz);
+    return brewAmountOz;
   }
 
-//this is not being called right now
-  setIngredients(brewAmount, goldenRatio, servingSize=280) {   
-    const waterGrams = Math.round(brewAmount * servingSize);
+//convert water in oz to grams
+  waterOzToGrams (waterOz) {
+    const waterGrams = Math.round(waterOz * waterGramsPerOz);
+    console.log(waterGrams);
+    return waterGrams;
+  }
+
+//use the goldenRatio and water amount to calculate cofee needed in grams;
+  coffeeGrams (waterGrams, goldenRatio) {
     const coffeeGrams = Math.round(waterGrams / goldenRatio);
-
-    this.setState({
-        brewAmount: brewAmount,
-        coffeeGrams: coffeeGrams,
-        waterGrams: waterGrams
-    });
+    return coffeeGrams;
   }
+
+// //this is not being called right now
+//   setIngredients(brewAmount, goldenRatio, servingSize=280) {   
+//     const waterGrams = Math.round(brewAmount * servingSize);
+//     const coffeeGrams = Math.round(waterGrams / goldenRatio);
+
+//     this.setState({
+//         brewAmount: brewAmount,
+//         coffeeGrams: coffeeGrams,
+//         waterGrams: waterGrams
+//     });
+//   }
 
   render() {
     const { brewAmount, coffeeGrams, waterGrams, goldenRatio } = this.state;
@@ -92,20 +134,26 @@ class App extends Component {
             <IncrementDecrementSet
               name="brewAmount"
               title="Brew Amount"
-              value={`${brewAmount} (oz)`}
-              setQuantity={this.setQuantity}
+              value={brewAmount}
+              measure="oz"
+              setQuantity=
+                {this.updateQuantity}
             />
             <IncrementDecrementSet
               name="coffeeGrams"
               title="Ground Coffee"
-              value={`${coffeeGrams} (g)`}
-              setQuantity={this.setQuantity}
+              value={coffeeGrams}
+              measure="g"
+              setQuantity=
+                {this.updateQuantity}
             />
             <IncrementDecrementSet
               name="waterGrams"
               title="Water"
-              value={`${waterGrams} (g/mL)`}
-              setQuantity={this.setQuantity}
+              value={waterGrams}
+              measure="g/mL"
+              setQuantity=
+                {this.updateQuantity}
             />
           </div>
 
