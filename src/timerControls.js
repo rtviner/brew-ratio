adcf6320aac2cbfcec7d32e7e74762a9472a6af3
Rebecca Viner <rtviner@gmail.com>
2019-09-27 17:29:34 -0600

    add timer controls

---
+++ src/timerControls.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '@fortawesome/fontawesome-free/js/solid';
+
+const Controls = ({ playPauseClick, resetClick }) => (
+    <div className="controls">
+        <button
+            id="start_stop"
+            onClick={playPauseClick}
+        >
+            <i className="fa fa-play" aria-hidden="true"/>
+            <i className="fa fa-pause" aria-hidden="true"/>
+        </button>
+        <button
+            id="reset"
+            onClick={resetClick}
+        >
+            <i className="fa fa-refresh" aria-hidden="true"/>
+        </button>
+    </div>
+);
+
+Controls.propTypes = {
+    playPauseClick: PropTypes.func,
+    resetClick: PropTypes.func
+};
+
+export { Controls };
