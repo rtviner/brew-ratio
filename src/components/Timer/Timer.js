ed9c68f3e34df20221c4823ca203eed4cd00e03d
Rebecca Viner <rtviner@gmail.com>
2019-10-21 18:14:18 -0600

    update timer state and methods to seconds only

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,16 +1,85 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Controls from './Controls';
 import TimerDisplay from './TimerDisplay';
 import IncrementDecrementButton from '../shared/IncrementDecrementButton';
 
-let twoDigits = time => (time >= 10 ? time : `0${time}`);
+const Timer = () => {
+  const DefaultSeconds = 180;
+  let twoDigits = time => (time >= 10 ? time : `0${time}`);
 
-const countdownView = (minutes, seconds) => `${twoDigits(minutes) || '00'}:${twoDigits(seconds) || '00'}`;
+  const countdownView = (seconds) => {
+    let minutes = Math.floor(seconds/60);
+    let secondsLeft = seconds % 60;
 
-const Timer = ({ stepUpTime, stepDownTime, minutes, seconds, playPause, resetTimer  }) => (
+    return `${twoDigits(minutes) || '00'}:${twoDigits(secondsLeft) || '00'}`; 
+  } 
+  
 
+  const [seconds, setSeconds] = useState(DefaultSeconds);
+  const [timerOn, setTimerOn] = useState(false);
+  
+  // useEffect(() => {
+  //   const seconds = parseFloat(localStorage.getItem('seconds'))
+  //       || DefaultSeconds;
+
+  //   setSeconds(seconds);
+  // }, []);
+
+  useEffect(() => {
+    var tick = null;
+    
+    if (timerOn && seconds > 0) {
+      let endTime = new Date().getTime() +seconds * 1000 + 500;
+
+      const updateClock = () => {
+        let msLeft = endTime - new Date().getTime();
+
+        if (msLeft >= 0) {
+          setSeconds(Math.floor(msLeft/1000));
+
+          tick = setTimeout(
+            updateClock,
+            new Date(msLeft).getUTCMilliseconds() + 500
+          );
+        }
+      }
+      updateClock()
+    } else if (!timerOn) {
+      clearInterval(tick);
+    }
+    return () => clearInterval(tick);
+  }, [timerOn, seconds]);
+
+  const stepUpTime = () => {
+    const currentSeconds = seconds;
+
+    if (!timerOn && currentSeconds < 420) {
+      let newSeconds = currentSeconds + 1;
+      setSeconds(newSeconds);
+    }
+  }
+
+  const stepDownTime = () => {
+    const currentSeconds = seconds;
+
+    if (!timerOn && seconds > 0) {
+      let newSeconds = currentSeconds - 1;
+      setSeconds(newSeconds);
+    }
+  };
+
+  const resetTimer = () => {
+    setSeconds(DefaultSeconds);
+    setTimerOn(false);
+  }
+
+  const playPause = () => {
+    setTimerOn(!timerOn);
+  }
+
+  return (
     <div id="countdown-timer">
       <div className="increment timer">
         <IncrementDecrementButton 
@@ -24,13 +93,14 @@ const Timer = ({ stepUpTime, stepDownTime, minutes, seconds, playPause, resetTim
           text="-"
         />
       </div>
-      <TimerDisplay time={countdownView(minutes, seconds)} />
+      <TimerDisplay time={countdownView(seconds)} />
       <Controls
         playPauseClick={playPause}
         resetClick={resetTimer}
       />
     </div>
-);
+  );
+};
 
 Timer.propTypes = {
     stepUpTime: PropTypes.func,
