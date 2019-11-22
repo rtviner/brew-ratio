786a7142038f35ef21ed6649bd1fd6acf71e4caa
Rebecca Viner <rtviner@gmail.com>
2019-11-22 11:36:02 -0700

    move constants and util functions to directories

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+
+const aeroCupSize = 210;
+const DefaultCupSize = 279;
+const DefaultMethod = "Pour Over";
+const DefaultRatio = 15.5;
+const DefaultSeconds = 240;
+const DefaultWater = 558;
+
+export {aeroCupSize, DefaultCupSize, DefaultMethod,DefaultRatio, DefaultSeconds, DefaultWater};
