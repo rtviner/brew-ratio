dbb48592f9dcf7ec8a1b53a3619ce6065172e07d
Rebecca Viner <rtviner@gmail.com>
2019-11-22 11:34:16 -0700

    change settings based on method input

--- src/App.js
+++ src/App.js
@@ -1,142 +1,160 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import './App.css';
-import {DefaultMethod, DefaultWater, DefaultRatio, DefaultSeconds, cupSize} from './defaultValues.js'
-import {stepDownValue, stepUpValue} from './stepValue.js'
+import './App.css'
 
 import BrewMethodInput from './components/BrewMethodInput'
-import QuantityInput from './components/QuantityInput';
-import StrengthInput from './components/StrengthInput';
-import Timer from './components/Timer';
+import QuantityInput from './components/QuantityInput'
+import StrengthInput from './components/StrengthInput'
+import Timer from './components/Timer'
+import {
+  aeroCupSize,
+  DefaultCupSize,
+  DefaultMethod,
+  DefaultRatio,
+  DefaultSeconds,
+  DefaultWater,
+} from './constants'
+import { stepDownValue, stepUpValue } from './utils'
 
 const App = () => {
-  const [method, setMethod] = useState(DefaultMethod);
+  const [cupSize, setCupSize] = useState(DefaultCupSize);
   const [goldenRatio, setGoldenRatio] = useState(DefaultRatio);
-  const [waterGrams, setWaterGrams] = useState(DefaultWater);
+  // const [instructions, setInstructions] = useState(pourOverInstructions);
+  const [method, setMethod] = useState(DefaultMethod);
   const [seconds, setSeconds] = useState(DefaultSeconds);
   const [timerOn, setTimerOn] = useState(false);
-  
-  useEffect(() => {
-    const method = localStorage.getItem('method') || DefaultMethod;
-    const goldenRatio = parseFloat(localStorage.getItem('goldenRatio')) || DefaultRatio;
-    const waterGrams = parseFloat(localStorage.getItem('waterGrams'))
-        || DefaultWater;
-    const seconds = parseFloat(localStorage.getItem('seconds'))
-        || DefaultSeconds;
-
-    setMethod(method);
-    setGoldenRatio(goldenRatio);
-    setWaterGrams(waterGrams);
-    setSeconds(seconds);
-  }, []);
+  const [waterGrams, setWaterGrams] = useState(DefaultWater);
 
   useEffect(() => {
-    var tick = null;
-    
+    const method = localStorage.getItem('method') || DefaultMethod
+    const goldenRatio =
+      parseFloat(localStorage.getItem('goldenRatio')) || DefaultRatio
+    const waterGrams =
+      parseFloat(localStorage.getItem('waterGrams')) || DefaultWater
+    const seconds =
+      parseFloat(localStorage.getItem('seconds')) || DefaultSeconds
+
+    setMethod(method)
+    setGoldenRatio(goldenRatio)
+    setWaterGrams(waterGrams)
+    setSeconds(seconds)
+  }, [])
+
+  useEffect(() => {
+    var tick = null
+
     if (timerOn && seconds > 0) {
-      let endTime = new Date().getTime() +seconds * 1000 + 500;
+      let endTime = new Date().getTime() + seconds * 1000 + 500
 
       const updateClock = () => {
-        let msLeft = endTime - new Date().getTime();
+        let msLeft = endTime - new Date().getTime()
 
         if (msLeft >= 0) {
-          setSeconds(Math.floor(msLeft/1000));
+          setSeconds(Math.floor(msLeft / 1000))
 
           tick = setTimeout(
             updateClock,
             new Date(msLeft).getUTCMilliseconds() + 500
-          );
+          )
         }
       }
       updateClock()
     } else if (!timerOn) {
-      clearInterval(tick);
+      clearInterval(tick)
     }
-    return () => clearInterval(tick);
-  }, [timerOn, seconds]);
+    return () => clearInterval(tick)
+  }, [timerOn, seconds])
 
-  const stepTime = (event) => {
+  const stepTime = event => {
     if (!timerOn && seconds < 420) {
-      let newSeconds = 
-        (event.target.innerText === '-') ? 
-        stepDownValue(seconds, 1) :
-        stepUpValue(seconds, 1);
-      setSeconds(newSeconds);
-    }  
+      let newSeconds =
+        event.target.innerText === '-'
+          ? stepDownValue(seconds, 1)
+          : stepUpValue(seconds, 1)
+      setSeconds(newSeconds)
+    }
   }
 
   const resetTimer = () => {
-    setSeconds(DefaultSeconds);
-    setTimerOn(false);
-  };
+    setSeconds(DefaultSeconds)
+    setTimerOn(false)
+  }
 
   const playPause = () => {
-    setTimerOn(!timerOn);
-  };
+    setTimerOn(!timerOn)
+  }
 
-  const updateGoldenRatio = (event) => {
-    let ratio = event.target.value;
-    setGoldenRatio(parseFloat(ratio));
-  };
+  const updateGoldenRatio = event => {
+    let ratio = event.target.value
+    setGoldenRatio(parseFloat(ratio))
+  }
 
   const conversionFactors = {
     cups: cupSize,
     coffeeGrams: goldenRatio,
-    waterGrams: 1
-  };
+    waterGrams: 1,
+  }
 
-  const updateWater = (event) => {
-    const eventInfo = event.target.id.split('-');
-    let conversionFactor = conversionFactors[eventInfo[0]];
-    let incrementFactor = (eventInfo[0] === 'cups') ? conversionFactor/4 : conversionFactor;
-    let value = event.target.type === 'number' ? event.target.value : waterGrams;
-    let newWater;
+  const updateWater = event => {
+    const eventInfo = event.target.id.split('-')
+    let conversionFactor = conversionFactors[eventInfo[0]]
+    let incrementFactor =
+      eventInfo[0] === 'cups' ? conversionFactor / 4 : conversionFactor
+    let value = event.target.type === 'number' ? event.target.value : waterGrams
+    let newWater
 
-    if (eventInfo[1] === 'decrement') { 
-      newWater = stepDownValue(value,incrementFactor);
+    if (eventInfo[1] === 'decrement') {
+      newWater = stepDownValue(value, incrementFactor)
     } else if (eventInfo[1] === 'increment') {
-      newWater = stepUpValue(value,incrementFactor);
+      newWater = stepUpValue(value, incrementFactor)
     } else if (eventInfo[1] === 'amount') {
-      newWater = value * conversionFactor;
-    };
-    setWaterGrams(newWater);
-  };
+      newWater = value * conversionFactor
+    }
+    setWaterGrams(newWater)
+  }
 
-  const changeMethod = (event) => {
-    let method = event.target.value;
+  const changeMethod = event => {
+    let method = event.target.value
+    let cupSize = (method === 'AeroPress') ? aeroCupSize : DefaultCupSize;
+    let ratio = (method === 'AeroPress') ? 13 : DefaultRatio;
+    let water = (method === 'AeroPress') ? 220 : DefaultWater;
+    let brewTime = (method === 'AeroPress') ? 120 : DefaultSeconds;
+    setCupSize(cupSize);
+    setGoldenRatio(ratio);
     setMethod(method);
-  };
+    setSeconds(brewTime);
+    setWaterGrams(water);
+  }
 
   const saveSettings = () => {
-    localStorage.setItem('method', method);
-    localStorage.setItem('goldenRatio', 
-      goldenRatio);
-    localStorage.setItem('waterGrams', 
-      waterGrams);
+    localStorage.setItem('method', method)
+    localStorage.setItem('goldenRatio', goldenRatio)
+    localStorage.setItem('waterGrams', waterGrams)
     localStorage.setItem('seconds', seconds)
-  };
+  }
 
   return (
     <div className="App">
       <main>
         <BrewMethodInput
-          method = {method}
-          changeMethod = {changeMethod}
+          method={method}
+          changeMethod={changeMethod}
         />
-        <StrengthInput 
-          goldenRatio = {goldenRatio}
-          setGoldenRatio = {updateGoldenRatio}
+        <StrengthInput
+          goldenRatio={goldenRatio}
+          setGoldenRatio={updateGoldenRatio}
         />
-        <QuantityInput 
-          waterGrams = {waterGrams}
-          goldenRatio = {goldenRatio}
-          updateWater = {updateWater}
+        <QuantityInput
+          waterGrams={waterGrams}
+          goldenRatio={goldenRatio}
+          cupSize={cupSize}
+          updateWater={updateWater}
         />
-        <Timer 
-          seconds = {seconds}
-          stepTime = {stepTime}
-          resetTimer = {resetTimer}
-          playPause = {playPause}
+        <Timer
+          seconds={seconds}
+          stepTime={stepTime}
+          resetTimer={resetTimer}
+          playPause={playPause}
         />
         <button
           id="saveSettings"
@@ -148,7 +166,7 @@ const App = () => {
         </button>
       </main>
     </div>
-  );
-};
+  )
+}
 
 export default App
