e7fc8d87d363668c183a78a2761990b482ad5063
Rebecca Viner <rtviner@gmail.com>
2019-11-22 11:35:21 -0700

    pass cupSize in as a prop for quantity views

--- src/components/QuantityInput/QuantityInput.js
+++ src/components/QuantityInput/QuantityInput.js
@@ -1,33 +1,32 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-
 import IncrementDecrementQuantity from './IncrementDecrementQuantity'
 
-const cupsFromWater = (water, cupSize = 280) => {
+const cupsFromWater = (cupSize, water) => {
   const possibleCups = water / cupSize
   const roundedPossibleCups = Math.round(possibleCups * 4) / 4
   return roundedPossibleCups
 };
 
-const coffeeGramsFromWater = (water, goldenRatio) => {
+const coffeeGramsFromWater = (goldenRatio, water) => {
   const coffeeGrams = water / goldenRatio
   return Math.round(coffeeGrams)
 };
 
 //wrap this in form not div
-const QuantityInput = ({ waterGrams, goldenRatio, updateWater }) => (
+const QuantityInput = ({ cupSize, waterGrams, goldenRatio, updateWater }) => (
     <div className="adjustables"> 
       <IncrementDecrementQuantity
         name="cups"
         title="Brewed Cups"
-        value={cupsFromWater(waterGrams)}
+        value={cupsFromWater(cupSize, waterGrams)}
         measure="8oz"
         changeQuantity={updateWater}
       />
       <IncrementDecrementQuantity
         name="coffeeGrams"
         title="Ground Coffee"
-        value={coffeeGramsFromWater(waterGrams, goldenRatio)}
+        value={coffeeGramsFromWater(goldenRatio,waterGrams)}
         measure="g"
         changeQuantity={updateWater}
       />
@@ -42,6 +41,7 @@ const QuantityInput = ({ waterGrams, goldenRatio, updateWater }) => (
 );
 
 QuantityInput.propTypes = {
+    cupSize: PropTypes.number,
     waterGrams: PropTypes.number,
     goldenRatio: PropTypes.number,
     updateWater: PropTypes.func
