47861849cbf3941face4b65d975a7d454fd24b56
Rebecca Viner <rtviner@gmail.com>
2019-12-02 17:14:39 -0700

    add brew instructions styles

--- src/defaultValues.js
+++
@@ -1,7 +0,0 @@
-const DefaultMethod = "Pour Over"
-const DefaultWater = 558
-const DefaultRatio = 15.5
-const DefaultSeconds = 180;
-const cupSize = 279;
-
-export {DefaultMethod, DefaultWater, DefaultRatio, DefaultSeconds, cupSize};
