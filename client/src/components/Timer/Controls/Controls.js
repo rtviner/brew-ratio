3f83bcc3c21b50cf875780d30cc3a477085b4d08
Rebecca Viner <rtviner@gmail.com>
2019-12-18 20:40:05 -0700

    update modules for production

--- client/src/components/Timer/Controls/Controls.js
+++ client/src/components/Timer/Controls/Controls.js
@@ -1,6 +1,8 @@
-import '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import React from 'react';
+
+import { faPlay, faPause, faRefresh } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Controls = ({ playPauseClick, resetClick }) => (
     <div className="controls">
@@ -8,14 +10,14 @@ const Controls = ({ playPauseClick, resetClick }) => (
             id="start_stop"
             onClick={playPauseClick}
         >
-            <i className="fas fa-play" aria-hidden="true"/>
-            <i className="fas fa-pause" aria-hidden="true"/>
+            <FontAwesomeIcon icon={faPlay} />
+            <FontAwesomeIcon icon={faPause} />
         </button>
         <button
             id="reset"
             onClick={resetClick}
         >
-            <i className="fas fa-refresh" aria-hidden="true"/>
+            <FontAwesomeIcon icon={faRefresh} />
         </button>
     </div>
 );
