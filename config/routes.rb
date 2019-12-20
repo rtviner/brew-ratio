029a8b3aa306314760657923805d24fc4dd5f2b2
Rebecca Viner <rtviner@gmail.com>
2019-12-20 13:20:17 -0700

    update for production add cors gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   namespace :api, :path => "", :constraints => {:subdoamin => "api"}, :default => {:format => :json} do
     resources :instructions
   end
+  # get "/*path", to: "react#index"
 end
